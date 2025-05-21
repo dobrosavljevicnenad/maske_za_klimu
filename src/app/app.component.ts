7e869b4e769283f5fb144b4723f201c2e39c66f2
ndobrosavljevic <n.dobrosavljevic01@gmail.com>
2025-05-22 00:00:32 +0200

    Revert "dodate neke stvarcice"
    This reverts commit 25e41ed46b61667210999189de857b0e761d8137.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Title, Meta } from '@angular/platform-browser';
-
 
 @Component({
   selector: 'app-root',
@@ -8,20 +6,5 @@ import { Title, Meta } from '@angular/platform-browser';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private titleService: Title, private metaService: Meta) {
-    // SEO TITLE
-    this.titleService.setTitle('Maske za klimu - Dekorativne i kvalitetne maske po meri');
-
-    // SEO META DESCRIPTION
-    this.metaService.updateTag({
-      name: 'description',
-      content: 'Kupite dekorativne maske za klimu koje ulepšavaju prostor i štite spoljašnju jedinicu. Prilagođene dimenzije, boje i dizajn po meri.'
-    });
-
-    // SEO META KEYWORDS (nije više presudan, ali može da pomogne)
-    this.metaService.updateTag({
-      name: 'keywords',
-      content: 'maske za klimu, dekorativne maske za klimu, zaštita klima uređaja, metalne maske, klima kutije, klima maska'
-    });
-  }
+  title = 'maske_za_klimu';
 }
