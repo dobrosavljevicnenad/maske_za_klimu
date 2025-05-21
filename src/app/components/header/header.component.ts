25e41ed46b61667210999189de857b0e761d8137
zpantovic <zaga123.zp@gmail.com>
2025-05-21 20:02:40 +0200

    dodate neke stvarcice

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -21,16 +21,16 @@ export class HeaderComponent {
   @Output() searchQueryChanged = new EventEmitter<string>();
 
   constructor(private maskaService: MaskaService,private title: Title, private meta: Meta) {
-       this.title.setTitle('Maske za klimu - Dekorativne, kvalitetne i povoljne maske za klimu');
-    this.meta.updateTag({ name: 'description', content: 'Kupite moderne maske za klimu. Ulepsajte svoj prostor elegantnim dekorativnim resenjima za spoljasnje jedinice klima uredjaja.' });
-    this.meta.updateTag({ name: 'keywords', content: 'maske za klimu, dekorativne maske za klimu, maske za spoljasnju jedinicu klime, klima maske, maske klima' });
-    this.cartCount$ = this.maskaService.cartCount$;
-    this.wishlistCount$ = this.maskaService.wishlistCount$;
-    this.cartProducts = this.maskaService.getCartProducts();
-
-    this.cartCount$.subscribe(() => {
+      this.title.setTitle('Maske za klimu - Dekorativne, kvalitetne i povoljne maske za klimu');
+      this.meta.updateTag({ name: 'description', content: 'Kupite moderne maske za klimu. Ulepsajte svoj prostor elegantnim dekorativnim resenjima za spoljasnje jedinice klima uredjaja.' });
+      this.meta.updateTag({ name: 'keywords', content: 'maske za klimu, dekorativne maske za klimu, maske za spoljasnju jedinicu klime, klima maske, maske klima' });
+      this.cartCount$ = this.maskaService.cartCount$;
+      this.wishlistCount$ = this.maskaService.wishlistCount$;
       this.cartProducts = this.maskaService.getCartProducts();
-    });
+
+      this.cartCount$.subscribe(() => {
+        this.cartProducts = this.maskaService.getCartProducts();
+      });
   }
 
   openModal() {
